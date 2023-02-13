8a4c7e9a2704c415153ca1cfccb957548fee96f9
Ma9lk <mayk1909@gmail.com>
2023-02-13 17:37:55 +0300

    change structure

--- pages/locators.py
+++ pages/locators.py
@@ -1,19 +1,19 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
 
-    LOGIN_LINK = (By.ID, 'login_link')
-
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 class LoginPageLocators():
 
     LOGIN_FORM = (By.ID, 'login-form')
     REGISTER_FORM = (By.ID, 'register-form')
 
-class ProdcutPageLocators():
+class ProductPageLocators():
 
     ADD_TO_BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')
     NAME_OF_BOOK = (By.CSS_SELECTOR, '.product_main h1')
-    TEXT_IN_SUCCSESS_MESSAGE = (By.CSS_SELECTOR, '.alert-success:nth-child(1) strong')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '.alert-success:first-child strong')
     PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main > .price_color')
-    PRODUCT_PRICE_IN_BASKET = (By.CSS_SELECTOR, '.alert-info strong')
+    PRODUCT_PRICE_IN_BASKET_MESSAGE = (By.CSS_SELECTOR, '.alert-info strong')
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-from .locators import ProdcutPageLocators
+from .locators import ProductPageLocators
 import re
 
 
@@ -7,23 +7,29 @@ import re
 class ProductPage(BasePage):
 
     def should_be_add_to_basket(self):
-        add_to_basket_click = self.browser.find_element(*ProdcutPageLocators.ADD_TO_BASKET)
+        add_to_basket_click = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
         add_to_basket_click.click()
 
     def should_be_name_of_book(self):
-        name_of_book = self.browser.find_element(*ProdcutPageLocators.NAME_OF_BOOK)
-        name_of_book.text
+        name_of_book = self.browser.find_element(*ProductPageLocators.NAME_OF_BOOK)
+        return name_of_book.text
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
 
     def should_be_succsess_message(self):
-        text_in_succsess_message = self.browser.find_element(*ProdcutPageLocators.TEXT_IN_SUCCSESS_MESSAGE)
-        text_in_succsess_message.text
+        text_in_succsess_message = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE)
+        return text_in_succsess_message.text
+
+    def should_be_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            'Success message is disappeared, but should not be'
 
     def should_be_product_price(self):
-        product_price = self.browser.find_element(*ProdcutPageLocators.PRODUCT_PRICE)
-        text_product_price = product_price.text
-        return text_product_price
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        return product_price.text
 
     def should_be_product_price_in_basket(self):
-        product_price_in_basket = self.browser.find_element(*ProdcutPageLocators.PRODUCT_PRICE_IN_BASKET)
-        text_product_price_in_basket = product_price_in_basket.text
-        return text_product_price_in_basket
+        product_price_in_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_BASKET_MESSAGE)
+        return product_price_in_basket.text
