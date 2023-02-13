9f1611f6a0d49be3f2e0266096d7eef32f913e7d
Ma9lk <mayk1909@gmail.com>
2023-02-13 17:37:23 +0300

    add new tests

--- test_product_page.py
+++ test_product_page.py
@@ -3,18 +3,59 @@ from .pages.base_page import BasePage
 from .pages.product_page import ProductPage
 import time
 
+# @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+#                                 pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+# def test_guest_can_add_product_to_basket(browser, link):
+#     # link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6'
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_be_add_to_basket()
+#     alert_answer = BasePage(browser, link)
+#     alert_answer.solve_quiz_and_get_code()
+#     assert page.should_be_name_of_book() == page.should_be_succsess_message(), \
+#         f'Название продукта {page.should_be_name_of_book()} не совпадает c текстом в сообщении {page.should_be_succsess_message()}'
+#     assert page.should_be_product_price() == page.should_be_product_price_in_basket(), \
+#         f'Цена продукта {page.should_be_product_price} не совпадает c ценой в корзине {page.should_be_product_price_in_basket()}'
 
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+# @pytest.mark.xfail
+# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+#     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6'
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_be_add_to_basket()
+#     page.should_not_be_success_message()
+
+# def test_guest_cant_see_success_message(browser):
+#     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6'
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_not_be_success_message()
+
+# @pytest.mark.xfail
+# def test_message_disappeared_after_adding_product_to_basket(browser):
+#     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6'
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_be_add_to_basket()
+#     page.should_be_disappeared_success_message()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
-    page.should_be_add_to_basket()
-    alert_answer = BasePage(browser, link)
-    alert_answer.solve_quiz_and_get_code()
-    assert page.should_be_name_of_book() == page.should_be_succsess_message(), \
-        f'Название продукта {page.should_be_name_of_book()} не совпадает c текстом в сообщении {page.should_be_succsess_message()}'
-    assert page.should_be_product_price() == page.should_be_product_price_in_basket(), \
-        f'Цена продукта {page.should_be_product_price} не совпадает c ценой в корзине {page.should_be_product_price_in_basket()}'
+    page.should_be_login_link()
+    
 
-    
-    
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()    
