24c368a5ca342965b861079fa8be211a4a09a404
Ma9lk <mayk1909@gmail.com>
2023-02-15 18:32:55 +0300

    add new locator in LoginPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -4,11 +4,17 @@ from selenium.webdriver.common.by import By
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, '.btn-group')
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class LoginPageLocators():
 
     LOGIN_FORM = (By.ID, 'login-form')
     REGISTER_FORM = (By.ID, 'register-form')
+    REGISTER_FORM_EMAIL = (By.ID, 'id_registration-email')
+    REGISTER_FORM_PASSWORD = (By.ID, 'id_registration-password1')
+    REPEAT_REGISTER_FORM_PASSWORD = (By.ID, 'id_registration-password2')
+    REGISTRATION_BUTTON = (By.CSS_SELECTOR, '[value="Register"]')
 
 class ProductPageLocators():
 
@@ -17,3 +23,8 @@ class ProductPageLocators():
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, '.alert-success:first-child strong')
     PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main > .price_color')
     PRODUCT_PRICE_IN_BASKET_MESSAGE = (By.CSS_SELECTOR, '.alert-info strong')
+
+class BasketPageLocators():
+
+    NOT_EMPTY_BASKET = (By.CSS_SELECTOR, '.basket-items')
+    TEXT_ABOUT_EMPTY_BASKET = (By.CSS_SELECTOR, '#content_inner > p')
