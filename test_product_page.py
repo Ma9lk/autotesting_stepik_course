0500277829f0a79a97c3bd23045276b037b30adb
Ma9lk <mayk1909@gmail.com>
2023-02-15 18:36:53 +0300

    add new class TestUserAddToBasketFromProductPage and comment other tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,29 +1,10 @@
 import pytest
 from .pages.base_page import BasePage
+from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 import time
 
-# @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-#                                 pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-# def test_guest_can_add_product_to_basket(browser, link):
-#     # link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6'
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.should_be_add_to_basket()
-#     alert_answer = BasePage(browser, link)
-#     alert_answer.solve_quiz_and_get_code()
-#     assert page.should_be_name_of_book() == page.should_be_succsess_message(), \
-#         f'Название продукта {page.should_be_name_of_book()} не совпадает c текстом в сообщении {page.should_be_succsess_message()}'
-#     assert page.should_be_product_price() == page.should_be_product_price_in_basket(), \
-#         f'Цена продукта {page.should_be_product_price} не совпадает c ценой в корзине {page.should_be_product_price_in_basket()}'
 
 # @pytest.mark.xfail
 # def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
@@ -31,12 +12,6 @@ import time
 #     page = ProductPage(browser, link)
 #     page.open()
 #     page.should_be_add_to_basket()
-#     page.should_not_be_success_message()
-
-# def test_guest_cant_see_success_message(browser):
-#     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6'
-#     page = ProductPage(browser, link)
-#     page.open()
 #     page.should_not_be_success_message()
 
 # @pytest.mark.xfail
@@ -47,15 +22,50 @@ import time
 #     page.should_be_add_to_basket()
 #     page.should_be_disappeared_success_message()
 
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
-    
+# def test_guest_should_see_login_link_on_product_page(browser):
+#     link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_be_login_link()
 
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()    
+# def test_guest_can_go_to_login_page_from_product_page(browser):
+#     link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/hacking-exposed-wireless_208/'
+#     page = BasePage(browser, link)
+#     page.open()
+#     page.go_to_basket_page()
+#     basket_page = BasketPage(browser, link)
+#     basket_page.should_be_empty_basket()
+#     basket_page.should_be_text_that_basket_is_empty()
+
+
+@pytest.mark.registration_user
+
+class TestUserAddToBasketFromProductPage():
+
+    link = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        email = str(time.time()) + "@fakemail.org"
+        password = 'EOdqA3Y@|v'
+        page = LoginPage(browser, self.link)
+        page.open()
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+
+        page = ProductPage(browser, self.link)
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+
+        link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/hacking-exposed-wireless_208/'
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_add_to_basket()
+        assert page.should_be_name_of_book() == page.should_be_succsess_message(), \
+            f'Название продукта {page.should_be_name_of_book()} не совпадает c текстом в сообщении {page.should_be_succsess_message()}'
+        assert page.should_be_product_price() == page.should_be_product_price_in_basket(), \
+            f'Цена продукта {page.should_be_product_price} не совпадает c ценой в корзине {page.should_be_product_price_in_basket()}'
