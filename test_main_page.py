d9c82822f89cf189a50d6293fbdbbd42dc221942
Ma9lk <mayk1909@gmail.com>
2023-02-15 18:48:44 +0300

    add marks

--- test_main_page.py
+++ test_main_page.py
@@ -24,13 +24,13 @@ class TestLoginFromMainPage():
         login_page = LoginPage(browser, link)
         login_page.should_be_login_url()
 
-
-# def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-#     link = 'http://selenium1py.pythonanywhere.com'
-#     page = BasePage(browser, link)
-#     page.open()
-#     page.go_to_basket_page()
-#     basket_page = BasketPage(browser, link)
-#     basket_page.should_be_empty_basket()
-#     basket_page.should_be_text_that_basket_is_empty()
+@pytest.mark.skip
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com'
+    page = BasePage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, link)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_text_that_basket_is_empty()
 
